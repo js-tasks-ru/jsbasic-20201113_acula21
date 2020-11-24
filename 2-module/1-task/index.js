b38c721eac75ece83fbdb52d896ff8cb0db327d7
CrispyMoses <Gendelf4@yandex.ru>
2020-11-25 01:51:31 +0300

    Занятие 2: Объекты

---
+++ 2-module/1-task/index.js
@@ -0,0 +1,14 @@
+/**
+ * Складываем зарплаты
+ * @param {Object} salaries - объект зарплат
+ * @returns {Number}
+ */
+function sumSalary(salaries) {
+  // ваш код...
+  let a = 0;
+  for (let key in salaries) {
+    !!(+salaries[key]) === true ? salaries[key] === undefined ? a = 0 : a += salaries[key] : a;
+  }
+  return a;
+}
+
