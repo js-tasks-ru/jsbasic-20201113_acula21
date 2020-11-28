4cad1c3bfef5f2e7d08698c0d7dcc26c6013d2d9
Toex-x <59730894+Toex-x@users.noreply.github.com>
2020-11-28 23:41:06 +0300

    Update index.js

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,4 +5,12 @@
  */
 function getMinMax(str) {
   // ваш код...
+  let arr = [], obj = {};
+  let num = str.replace(/[^0-9\-\+\s\.\,]/g, '').replace(/ {2,}/g, ',').split(',');
+  for(let a of num)
+    if(!isNaN(a) && '' !== a) arr.push(+a);
+
+  obj.min = Math.min(...arr);
+  obj.max = Math.max(...arr);
+  return obj;
 }
