245f6be35c71a81fbc53ea3e1a71f18ba77b534a
CrispyMoses <Gendelf4@yandex.ru>
2020-11-29 16:30:45 +0300

    Занятие 3: Функции и массивы

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -6,13 +6,9 @@
 
 function getMinMax(str) {
   // ваш код...
-
-  let arr = [], obj = {};
-  let num = str.replace(/[^0-9\-\+\s\.\,]/g, '').replace(/ {2,}/g, ',').split(',');
-  for(let a of num)
-    if(!isNaN(a) && '' !== a) arr.push(+a);
-
-  obj.min = Math.min(...arr);
-  obj.max = Math.max(...arr);
-  return obj;
+  let num = str.replace(/[a-zа-яё\+\s]/g, ',').split(',');
+  return {
+    min: Math.min(...num),
+    max: Math.max(...num)
+  };
 }
