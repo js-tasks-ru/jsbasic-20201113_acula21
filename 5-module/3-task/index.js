f4cd006b26623e4b6cd9e487a0146d5fa2bffcd3
CrispyMoses <Gendelf4@yandex.ru>
2020-12-09 13:50:17 +0300

    Занятие 3: Функции и массивы

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -6,23 +6,29 @@ function initCarousel() {
     let width = list.offsetWidth;
     let right = document.querySelector('.carousel__arrow_right');
     let left = document.querySelector('.carousel__arrow_left');
-    if (position === 0) left.style.display = 'none';
+    left.style.display = 'none';
     
 
     left.onclick = function() {
       position += width;
-      if(position === -2964) right.style.display = '' ;
-      if(position < 0) left.style.display = '';
-      if(position === 0) left.style.display = 'none';
+      
+      if (position < 0) {  
+        left.style.display = '';
+        right.style.display = '';
+      }
+      if (position === 0) left.style.display = 'none';
 
       list.style.transform = 'translateX(' + position + 'px)';
     };
 
     right.onclick = function() {
       position -= width;
-      if(position === -2964) right.style.display = 'none' ;
-      if(position < 0) left.style.display = '';
-      if(position === 0) left.style.display = 'none';
+      right.style.display = 'none' ;
+
+      if (position <= 0 && position > -2964) {
+        left.style.display = '';
+        right.style.display = '';
+      } 
 
       list.style.transform = 'translateX(' + position + 'px)';
     };
