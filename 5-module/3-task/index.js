2c7c2d006a490f2c457be4aa700134b081e145c7
CrispyMoses <Gendelf4@yandex.ru>
2020-12-09 13:03:08 +0300

    Занятие 5: Обработчики событий

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
+const { ConsoleReporter } = require("jasmine");
+
 function initCarousel() {
-  // ваш код...
+    let position = 0;
+    let list = document.querySelector('.carousel__inner');
+    let width = list.offsetWidth;
+    let right = document.querySelector('.carousel__arrow_right');
+    let left = document.querySelector('.carousel__arrow_left');
+    if (position === 0) left.style.display = 'none';
+    
+
+    left.onclick = function() {
+      position += width;
+      if(position === -2964) right.style.display = '' ;
+      if(position < 0) left.style.display = '';
+      if(position === 0) left.style.display = 'none';
+
+      list.style.transform = 'translateX(' + position + 'px)';
+    };
+
+    right.onclick = function() {
+      position -= width;
+      if(position === -2964) right.style.display = 'none' ;
+      if(position < 0) left.style.display = '';
+      if(position === 0) left.style.display = 'none';
+
+      list.style.transform = 'translateX(' + position + 'px)';
+    };
+
 }
